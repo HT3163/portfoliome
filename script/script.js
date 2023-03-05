60a4dd4ef173f04797570864df3892cb1c309fb8
HT3163 <84614610+HT3163@users.noreply.github.com>
2023-03-05 22:44:43 +0500

    change backgrounimage ratio from 0.07 to 0.10

--- script/script.js
+++ script/script.js
@@ -54,7 +54,7 @@ function setcolor(e) {
         r.style.setProperty('--blue', 'red');
         r.style.setProperty('--red', '#0582ff');
 
-        rr.style.backgroundImage = 'repeating-linear-gradient(45deg, rgb(255 255 255 / 90%) 0%, rgb(255 255 255 / 3%) 25%, transparent 25%, rgba(0, 0, 0, 0.07) 50%)'
+        rr.style.backgroundImage = 'repeating-linear-gradient(45deg, rgb(255 255 255 / 90%) 0%, rgb(255 255 255 / 3%) 25%, transparent 25%, rgba(0, 0, 0, 0.10) 50%)'
         rr.style.backgroundSize = '11rem'
 
     } else {
